f7a25781ccc22aa3302feb50d43f382dae4eae40
AndriiPotap <andrew.potapenko47@gmail.com>
2022-05-24 22:23:00 +0300

    add a chart

---
+++ src/Data.js
@@ -0,0 +1,26 @@
+export const UserData = [
+    {
+        name: 'January',
+        Total: 1200
+    },
+    {
+        name: 'February',
+        Total: 2100
+    },
+    {
+        name: 'March',
+        Total: 880
+    },
+    {
+        name: 'April',
+        Total: 1600
+    },
+    {
+        name: 'May',
+        Total: 900
+    },
+    {
+        name: 'June',
+        Total: 1700
+    }
+];
--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import './chart.scss';
+import { UserData } from '../../Data';
+
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+
 
 const Chart = () => {
+
   return (
     <div className='chart'>
-      Chart
+<div className="title">Last 6 Mounths (Revenue)</div>
+     <ResponsiveContainer width="100%" aspect={ 2/1}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={UserData}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" className='chartGrid'/>
+          <XAxis dataKey="name" stroke='gray' />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="Total" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
+
       </div>
   )
 }
